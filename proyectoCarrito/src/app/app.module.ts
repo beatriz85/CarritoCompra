bfdb23f61c91b893a041e6f449bafa4793bd020f
beatriz85 <beatriz.garcia.ruizadame@gmail.com>
2020-11-17 09:57:27 +0100

    Creado componentes carrito + producto

--- proyectoCarrito/src/app/app.module.ts
+++ proyectoCarrito/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarritoComponent } from './carrito/carrito.component';
+import { ProductosComponent } from './productos/productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarritoComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule
